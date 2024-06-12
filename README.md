00ca7b74140e74c4b66e47ef3a8078e3add99a76
farah <farahaliali346@gmail.com>
2024-06-13 00:51:22 +0300

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # wine-quality
+
+'''bash
+conda create --name mlprojectenv python 
+conda activate mlprojectenv
+'''
+'''bash
+python -m venv myenv
+'''
+
+'''bash
+pip install -r requirments.txt
+'''
--- template.py
+++ template.py
@@ -2,13 +2,15 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
 
-#create a projectname
 project_name='mlProject'
-list_of_files=[
+
+#list of files
+list_of_files = [
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/componenets/__init__.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
@@ -22,24 +24,28 @@ list_of_files=[
     "schema.yaml",
     "main.py",
     "app.py",
+    "Dockerfile",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "test.py"
 ]
 
 for filepath in list_of_files:
-    filepath=Path(filepath)
-    filedir,filename=os.path.split(filepath  )
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
-if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-    with open(filepath, "w") as f:
-        pass
-    logging.info(f"Creating empty file: {filepath}")
-else:
-    logging.info(f" file: {filename} is already exists ")
-    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
